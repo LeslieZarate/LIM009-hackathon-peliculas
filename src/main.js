da756b789bfaeac3fc637bd9b5cbb6aa9ebc2bff
LeslieZarate <fiorelly30zarate@gmail.com>
2019-07-06 19:58:12 -0500

    Cambiando algunos estilos

--- src/main.js
+++ src/main.js
@@ -1,39 +1,114 @@
+/* eslint-disable id-length */
 const btnSearch = document.getElementById('btn-search');
-const result = document.getElementById('result');
-const btnRating = document.getElementById('btn-rating');
 const inputQuery = document.getElementById('input-query');
-const inputQueryNav = document.getElementById('input-query2');
-const btnSearchNav = document.getElementById('btn-search2');
+
+const inputQueryNav = document.getElementById('input-query-nav');
+const btnSearchNav = document.getElementById('btn-search-nav');
+
+const resultSearch = document.getElementById('result-search');
+
+const detailMovie = document.getElementById('detail-movie');
+
+const btnRating = document.getElementById('btn-rating');
+
 const apiKey = 'apikey=f4578cd7';
 let dataSearched;
 
-btnSearch.addEventListener('click', () => {
-  if (inputQuery.value !== '') {
-  toggleDisplay();
+// Busqueda de la pagina inicial del app
+
+btnSearch.addEventListener('click', (e) => {
+  e.preventDefault();
   let query = inputQuery.value;
-  const url = `https://www.omdbapi.com/?&${apiKey}&s=${query}`;
-  fetchDataSearchTotal(url);
-  document.getElementById('buttons').classList.remove('d-none');
-  document.getElementById('result').classList.remove('d-none');
+  if (query !== '') {
+    e.preventDefault();
+    document.getElementById('navbar-handbook').classList.remove('d-none');
+    document.getElementById('main-form').classList.add('d-none');
+    document.getElementById('content-movies').classList.remove('d-none');
+    const url = `https://www.omdbapi.com/?&${apiKey}&s=${query}`;
+    fetchDataSearchTotal(url);
+  } else {
+    alert('Escribe algo');
   }
+  document.getElementById('main-form').reset();
 });
 
-btnSearchNav.addEventListener('click', () => {
+// Busqueda en el query del nav 
+btnSearchNav.addEventListener('click', (e) => {
+  document.getElementById('content-movies').classList.remove('d-none');
+  document.getElementById('detail-movie').classList.add('d-none');
+  e.preventDefault();
   if (inputQuery.value !== '') {
-  let query = inputQueryNav.value;
-  const url = `https://www.omdbapi.com/?&${apiKey}&s=${query}`;
-  fetchDataSearchTotal(url);
-}
+    let query = inputQueryNav.value;
+    const url = `https://www.omdbapi.com/?&${apiKey}&s=${query}`;
+    fetchDataSearchTotal(url);
+  }
+  document.getElementById('nav-form').reset();
 });
 
-btnRating.addEventListener('click', () => {
-  const dataOrdered = movie.sortData(dataSearched);
-  result.innerHTML = drawTemplate(dataOrdered);
-});
+// Funciones  obtener la data peliculas y pintado 
+const fetchDataSearchTotal = (url) => {
+  const page = 3;
+  let dataPages = [];
+  for (let i = 1; i <= page; i++) {
+    dataPages.push(fetch(`${url}&page=${[i]}`)
+      .then(response => response.json())
+      .then(data => data.Search)
+    );
+  }
+  Promise.all(dataPages)
+    .then(responses => responses[0].concat(responses[1], responses[2]))
+    .then(result => movie.getId(result))
+    .then(movie => {
+      let newData = [];
+      let movieId;
+      movieId = movie;
+      for (let i = 0; i < movieId.length; i++) {
+        newData.push(fetch(`https://www.omdbapi.com/?&${apiKey}&i=${movieId[i]}`).then(response => response.json()));
+      }
+      Promise.all(newData)
+        .then(responses => {
+          dataSearched = responses;
+          resultSearch.innerHTML = '';
+          resultSearch.appendChild(drawTemplate(dataSearched));
+        });
+    });
+};
 
-result.addEventListener('click', (event) => {
-  const url = `https://www.omdbapi.com/?&${apiKey}&t=${event.target.alt}`;
-  fetchDataDetails(url);
+const drawTemplate = (data) => {
+  const totalCard = document.createElement('div');
+  totalCard.classList.add('row', 'p-3', 'm-0', 'd-flex', 'justify-content-center');
+
+  for (let i = 0; i < data.length; i++) {
+    let card = `        
+        <div class="card border-dark col-12  col-sm-3 col-md-2 col-lg-2 my-3 mx-3 text-center">
+            ${data[i].Poster === 'N/A'
+    ? `<img class="card-img-top" src="./assets/poster.png" alt="${data[i].Title}">`
+    :
+    `<img class="card-img-top" src="${data[i].Poster}" alt="${data[i].Title}">`}
+            
+            <div class="card-body">
+            <p class="card-title text-info">${data[i].Title}</p>
+            <p class = "">${data[i].Year}</p>
+            </div>
+            <div class="card-footer  bg-dark">
+            <small class="text-white"><span class="text-warning">&#9733 </span> ${data[i].imdbRating}</small>
+            </div>
+      </div>
+    `;
+    totalCard.innerHTML += card;
+  }
+  return totalCard;
+};
+
+// Funciones  para la descripcion de cada pelicula
+
+resultSearch.addEventListener('click', (event) => {
+  if (event.target.alt !== undefined) {
+    const url = `https://www.omdbapi.com/?&${apiKey}&t=${event.target.alt}`;
+    fetchDataDetails(url);
+    document.getElementById('content-movies').classList.add('d-none');
+    document.getElementById('detail-movie').classList.remove('d-none');
+  }
 });
 
 const fetchDataDetails = (url) => {
@@ -41,77 +116,39 @@ const fetchDataDetails = (url) => {
     .then((response) => response.json())
     .then((data) => {
       const dataDetail = movie.getItemDetails(data);
-      result.innerHTML = drawDetailsTemplate(dataDetail);
+      detailMovie.innerHTML = ' ';
+      detailMovie.innerHTML = drawDetailsTemplate(dataDetail);
     });
 };
-
-const fetchDataSearchTotal = (url) => {
-  const page = 3; 
-  let dataPages = [];
-  for (let i = 1; i <= page; i++) {
-    dataPages.push(fetch(`${url}&page=${[i]}`)
-      .then(response=>response.json())
-      .then(data=>data.Search)
-    );
-  }
-  Promise.all(dataPages)
-    .then(responses =>responses[0].concat(responses[1], responses[2]))  
-    .then(result=> movie.getId(result))
-    .then(movie =>{ 
-      let newData = [];  
-      let movieId;         
-      movieId = movie;
-      for (let i = 0; i < movieId.length;i++) {
-        newData.push(fetch(`https://www.omdbapi.com/?&${apiKey}&i=${movieId[i]}`).then(response=>response.json()));
-      }
-      Promise.all(newData)
-        .then(responses => {
-          dataSearched = responses;
-          result.innerHTML = drawTemplate(dataSearched);
-        });
-    });
-};
-
-const drawTemplate = (data) => {
-  let totalCard = '<div class="row bg-dark p-3 m-0">';
-  for (let i = 0; i < data.length; i++) {
-    let card = `<div class="col-md-2 col-8 card-height mb-3 mx-auto">
-          <div class="card bg-dark b-4 shadow-sm h-100">
-            <a href="#"><img src="${data[i].Poster}" class="card-img-top" alt="${data[i].Title}"></a>
-            <div class="card-body p-0">
-              <small class="card-text">${data[i].Title}</small>
-              <div class="align-items-left">
-                <small class="text-muted">Rating: ${data[i].imdbRating}</small>
-              </div>
-            </div>
-          </div>
-        </div>`;
-    totalCard += card;
-  }
-  totalCard += '</div>';
-  return totalCard;
-};
-
-const toggleDisplay = () => {
-  document.getElementById('nav-form').classList.remove('d-none');
-  document.getElementById('main-form').classList.add('d-none');  
-}
 
 const drawDetailsTemplate = (obj) => {
-  let template = `<div class="container bg-dark container-details">
-                   <img class="rounded float-left h-100 w-auto" src="${obj.Poster}" alt="${obj.Title}" />
-                   <h3>${obj.Title}</h3>
-                   <p>Tipo: ${obj.Type}</p>
-                   <p>Rating: ${obj.imdbRating}</p>
-                   <p>Duración: ${obj.Runtime} min</p>
-                   <p>Género: ${obj.Genre}</p>
-                   <p>Actores: ${obj.Actors}</p>
-                   <p>Sinopsis: ${obj.Plot}</p>
-                 </div>`;
+  let template = ` 
+    <div class="container">
+      <div class="row my-5">
+        <div class="col-md-6  d-flex justify-content-center">
+            <img class="" src="${obj.Poster}" alt="${obj.Title}">
+        </div>
+        <div class="col-md-6 p-5">
+            <h5 class="text-center">${obj.Title}</h5>
+            <p class="">Tipo: ${obj.Type}</p>
+            <p class="">Rating: ${obj.imdbRating}</p>
+            <p class="">Duración: ${obj.Runtime} min</p>
+            <p class="">Género: ${obj.Genre}</p>
+            <p class="">Actores: ${obj.Actors}</p>
+            <p class="">Sinopsis: ${obj.Plot}</p>
+        </div>
+      </div> 
+    </div>    
+    `;
   return template;
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('nav-form').reset();
-  document.getElementById('main-form').reset();
+
+// Raiting
+btnRating.addEventListener('click', () => {
+  const dataOrdered = movie.sortData(dataSearched);
+  resultSearch.innerHTML = '';
+  resultSearch.appendChild(drawTemplate(dataOrdered));
 });
+
+
